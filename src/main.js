dcfb42fe38f72521b6f0d78d47bf19c1f34596d9
Cotbert2 <mateojgdev@gmail.com>
2024-11-05 10:15:29 -0500

    electron handle

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import { app, BrowserWindow, ipcMain } from 'electron'
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+        }
+    })
+//open dev tools
+    win.webContents.openDevTools();
+    win.loadFile('index.html')
+}
+
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') 
+          app.quit();
+    });
+})
+
+ipcMain.on('click', () => {
+    console.log('pong');
+});

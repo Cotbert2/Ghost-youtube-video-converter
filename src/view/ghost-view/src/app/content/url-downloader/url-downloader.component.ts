591fd5c855f5a372d4cd92fb40a00ba4b15c9cd3
Cotbert2 <mateojgdev@gmail.com>
2024-11-08 07:10:05 -0500

    content components

---
+++ src/view/ghost-view/src/app/content/url-downloader/url-downloader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-url-downloader',
+  standalone: true,
+  imports: [],
+  templateUrl: './url-downloader.component.html',
+  styleUrl: './url-downloader.component.scss'
+})
+export class UrlDownloaderComponent {
+
+}

3745e5f6ad443204ca267659744263e2fa50660a
Cotbert2 <mateojgdev@gmail.com>
2024-11-09 21:14:42 -0500

    url

--- src/view/ghost-view/src/app/app.component.ts
+++ src/view/ghost-view/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { ContentComponent } from "./content/content.component";
+import { FooterComponent } from './footer/footer.component';
+import { UrlDownloaderComponent } from "./content/url-downloader/url-downloader.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ContentComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, ContentComponent, UrlDownloaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/view/ghost-view/src/app/content/content.component.ts
+++ src/view/ghost-view/src/app/content/content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { UrlDownloaderComponent } from "./url-downloader/url-downloader.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [UrlDownloaderComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })

892e54cdc6a13f3fdb69939991cf690a1c37d484
Cotbert2 <mateojgdev@gmail.com>
2024-11-17 00:45:03 -0500

    steps aspect

--- src/view/ghost-view/src/app/content/content.component.ts
+++ src/view/ghost-view/src/app/content/content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UrlDownloaderComponent } from "./url-downloader/url-downloader.component";
+import { StepsComponent } from "./steps/steps.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [UrlDownloaderComponent],
+  imports: [UrlDownloaderComponent, StepsComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
